8b991ffb90218b487c0ea54b07d9668cfd4dbd1a
BRUNO BARRETO FERREIRA LEÃO <brunobarretoferreiraleao@gmail.com>
2026-05-08 23:45:33 -0300

    login page

---
+++ frontend/rafty/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/features/auth/components/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#071018] px-6">
+      <LoginForm />
+    </main>
+  );
+}
