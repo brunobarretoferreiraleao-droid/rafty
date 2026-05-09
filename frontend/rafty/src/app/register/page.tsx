70d5bcbc540b54eab30d7d34b87c8128f2e5610a
BRUNO BARRETO FERREIRA LEÃO <brunobarretoferreiraleao@gmail.com>
2026-05-08 23:57:02 -0300

    feat: register page

---
+++ frontend/rafty/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/features/auth/components/register-form";
+
+export default function RegisterPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#071018] px-6">
+      <RegisterForm />
+    </main>
+  );
+}
