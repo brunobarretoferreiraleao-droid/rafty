00a7f5a1cf7e602a1ff43b6672ec505591a29ff0
BRUNO BARRETO FERREIRA LEÃO <brunobarretoferreiraleao@gmail.com>
2026-05-08 23:12:13 -0300

    Register api

---
+++ frontend/rafty/src/features/auth/api/login.ts
@@ -0,0 +1,10 @@
+import { api } from "@/services/api";
+
+export async function login(data: {
+  email: string;
+  password: string;
+}) {
+  const response = await api.post("/auth/login", data);
+
+  return response.data;
+}
