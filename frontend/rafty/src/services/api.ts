bb609089d239479dbe7133a1861eeaed19f1f2fc
BRUNO BARRETO FERREIRA LEÃO <brunobarretoferreiraleao@gmail.com>
2026-05-10 13:54:48 -0300

    fix: api AuthStore

--- frontend/rafty/src/services/api.ts
+++ frontend/rafty/src/services/api.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
+import { useAuthStore } from "../features/auth/store/auth-store";
+
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
+  const token = useAuthStore.getState().token;
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
